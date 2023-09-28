82d563cfbafc379a1f669a66119a6957903cae80
AndyI0I <andreasffelder@hotmail.com>
2023-09-28 15:01:46 +0200

    stuff

--- source/Konto-App/src/main/java/com/ubs/configuration/DAOBuilder.java
+++ source/Konto-App/src/main/java/com/ubs/configuration/DAOBuilder.java
@@ -11,7 +11,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 
-public class DAOBuilder {
+public class  DAOBuilder {
 
     @Produces
     @Named("kontoDAO")
---
+++ source/Konto-App/src/main/java/com/ubs/data/dto/Account.java
@@ -0,0 +1,9 @@
+package com.ubs.data.dto;
+
+import java.math.BigDecimal;
+
+public class Account {
+
+    private int AccountId;
+    private BigDecimal Balance; 
+}
